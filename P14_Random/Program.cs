f3f696da07e73d992008fb4212bc1cd4f7aba94d
Audrius Miskinis <audriusmiskinis1@gmail.com>
2021-03-17 19:27:48 +0200

    random

--- P14_Random/Program.cs
+++ P14_Random/Program.cs
@@ -6,7 +6,7 @@ namespace P14_Random
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
+            /*Random random = new Random();
             int aRandomNumber = random.Next();
             int aRandomNumber1 = random.Next(4); // 0, 1, 2 arba 3
             int aRandomNumber2 = random.Next(1, 4); // 1, 2, 3
@@ -14,15 +14,48 @@ namespace P14_Random
 
             
             Metodas(random);
+            Console.WriteLine(bRandomNumber);*/
 
-            Console.WriteLine(bRandomNumber);
+            Kauliukas();
+
         }
 
-        public static int Metodas(Random rnd)
+        /*public static int Metodas(Random rnd) 
         {
             return rnd.Next(1, 10);      
-        }
+        }*/
 
-        
+        public static void Kauliukas()
+        {
+            Random random = new Random();
+            int kauliukoSkaicius = random.Next(1, 7);
+            Console.WriteLine($"{kauliukoSkaicius}");
+
+            switch (kauliukoSkaicius)
+            {
+                case 1:
+                    Console.WriteLine("Kartojame metima);
+                    Kauliukas();
+                    return;
+                case 2:
+                    Console.WriteLine("Deja pralaimejote");
+                    break;
+                case 3:
+                    Console.WriteLine("Deja pralaimejote");
+                    break;
+                case 4:
+                    Console.WriteLine("Deja pralaimejote");
+                    break;
+                case 5:
+                    Console.WriteLine("Sveikiname laimejote");
+                    break;
+                case 6:
+                    Console.WriteLine("Sveikiname laimejote");
+                    break;
+                default:
+                    Console.WriteLine("Pasirinkimas neteisingas");
+                    break;
+            }
+        }
     }
 }
